8f47a53de89c96ab9b619ee1aae37de3fbdf34af
adityamottha <adityamottha5@gmail.com>
2026-06-12 12:12:53 +0530

    First commit

---
+++ array/learn.array.js
@@ -0,0 +1,15 @@
+
+// traversy 1 question
+
+let example_array = [12,34,56,78,9,0]
+
+for (let i = 0; i < example_array.length; i++) {
+    const element = example_array[i];
+    // console.log(element);
+    
+}
+
+// Finding max and min in array
+
+
+
